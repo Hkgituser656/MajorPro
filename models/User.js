49b10a1de2e88b477c7be7d41360ed2855daad30
Harsh Kumar <149146548+Hkgituser656@users.noreply.github.com>
2024-11-02 21:16:04 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,8 +4,8 @@ const userSchema = new mongoose.Schema({
     name: String,
     email: { type: String, unique: true }, // Make email unique
     password: String,
-    engineeringYear: { type: Number, enum: [1, 2, 3, 4] }, // 1, 2, 3, or 4
-    domainOfInterest: { type: String, enum: ['Web Development', 'App Development', 'Machine Learning', 'Data Science', 'AI', 'Others'] },
+    level: { type: String, enum: ['beginner' ,'intermediate' ,'advanced'] }, // 1, 2, 3, or 4
+    domainOfInterest: { type: String, enum: ['Web Development', 'App Development', 'Machine Learning', 'JAVA', 'C++', 'DSA'] },
     languageMode: { type: String, enum: ['English', 'Hindi'] }
 });
 
