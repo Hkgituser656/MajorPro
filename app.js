bb9ce07404a486fb36d205d247a49a98cedfbf0d
Harsh Kumar <149146548+Hkgituser656@users.noreply.github.com>
2024-11-02 23:37:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -41,20 +41,36 @@ const videoLinks = {
             "Hindi": "."
         },
         "C++": {
-            "English": ".",
-            "Hindi": "."
+            "English": "https://www.youtube.com/watch?v=oOmbSpOzvYg&list=PLdo5W4Nhv31YU5Wx1dopka58teWP9aCee",
+            "Hindi": "https://www.youtube.com/watch?v=j8nAHeVKL08&list=PLu0W_9lII9agpFUAlPFe_VNSlXW5uE0YL"
         },
         "DSA": {
-            "English": ".",
-            "Hindi": "."
+            "English": "https://www.youtube.com/watch?v=AT14lCXuMKI&list=PLdo5W4Nhv31bbKJzrsKfMpo_grxuLl8LU",
+            "Hindi": "https://www.youtube.com/watch?v=y3OOaXrFy-Q&list=PLQEaRBV9gAFu4ovJ41PywklqI7IyXwr01"
         },
         
     },
     "intermediate": {
         //idhar bhi same upar k jaisa 
+        "C++": {
+            "English": "https://www.youtube.com/watch?v=oOmbSpOzvYg&list=PLdo5W4Nhv31YU5Wx1dopka58teWP9aCee",
+            "Hindi": "https://www.youtube.com/watch?v=WQoB2z67hvY&list=PLDzeHZWIZsToJ9zSl4-5BfOBzAR0fm--f"
+        },
+        "DSA": {
+            "English": "https://www.youtube.com/watch?v=0bHoB32fuj0&list=PLgUwDviBIf0oF6QL8m22w1hIDC1vJ_BHz",
+            "Hindi": "https://www.youtube.com/watch?v=5_5oE5lgrhw&list=PLu0W_9lII9ahIappRPN0MCAgtOu3lQjQi"
+        },
     },
     "advanced": {
         //idhar bhi same upar k jaisa 
+        "C++": {
+            "English": "https://www.youtube.com/watch?v=OAlU9IhJo2Y",
+            "Hindi": "https://www.youtube.com/watch?v=s3EMTa_p-Bs&list=PLy-CGmBdq2VGmVIOEO23mZgIUAyZVfODs"
+        },
+        "DSA": {
+            "English": "https://www.youtube.com/watch?v=rZ41y93P2Qo&list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ",
+            "Hindi": "https://www.youtube.com/watch?v=WQoB2z67hvY&list=PLDzeHZWIZsTryvtXdMr6rPh4IDexB5NIA"
+        },
     },
    
 };
